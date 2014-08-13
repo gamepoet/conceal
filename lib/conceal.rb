84c829cb7fd2485b659e52d77dfe0901e1aa93b4
Ben Scott <gamepoet@gmail.com>
2014-08-12 23:35:52 -0700

    initial implementation

--- lib/conceal.rb
+++ lib/conceal.rb
@@ -1,5 +1,89 @@
-require "conceal/version"
+require 'conceal/version'
+require 'base64'
+require 'openssl'
+require 'securerandom'
 
 module Conceal
-  # Your code goes here...
+  class << self
+    FORMAT_VERSION = 1
+
+    # Encrypts the given plaintext string.
+    #
+    # @param plaintext  [String]  the plaintext string to encrypt
+    # @param opts       [Hash]    additional options
+    #
+    # @option opts [String] :algorithm  the cipher algorithm to use (defaults to 'aes-25c-cbc')
+    # @option opts [String] :key        the secret shared key
+    def encrypt(plaintext, opts = {})
+      opts = {
+        algorithm: 'aes-256-cbc',
+      }.merge(opts)
+      key       = opts[:key]
+      algorithm = opts[:algorithm]
+      raise ArgumentError.new(':key option missing') if key.to_s.empty?
+      salt = SecureRandom.hex(128)
+
+      # setup the cipher
+      cipher = OpenSSL::Cipher::Cipher.new(algorithm)
+      cipher.encrypt
+      iv = cipher.random_iv
+      cipher.iv = iv
+      cipher.key = OpenSSL::PKCS5.pbkdf2_hmac_sha1(key, salt, 2000, cipher.key_len)
+
+      # encrypt
+      ciphertext = cipher.update(plaintext)
+      ciphertext << cipher.final
+
+      # MAC
+      digest = OpenSSL::Digest.new('sha256')
+      hmac = OpenSSL::HMAC.digest(digest, key, ciphertext)
+
+      [
+        FORMAT_VERSION,
+        algorithm,
+        encode64(iv),
+        salt,
+        encode64(hmac),
+        encode64(ciphertext),
+      ].join('$')
+    end
+
+    # Decrypts the given encrypted string.
+    #
+    # @param data [String]    the encrypted string to decrypt
+    # @param opts [Hash]      additional options
+    #
+    # @option opts [String] :key    the secret shared key
+    def decrypt(data, opts = {})
+      key = opts[:key]
+      raise ArgumentError.new(':key option missing') if key.to_s.empty?
+
+      ver, algorithm, iv64, salt, hmac64, ciphertext64 = data.split('$', 6)
+      raise ArgumentError.new('ciphertext has unknown version') unless ver == FORMAT_VERSION.to_s
+
+      iv         = Base64.decode64(iv64)
+      hmac       = Base64.decode64(hmac64)
+      ciphertext = Base64.decode64(ciphertext64)
+
+      # validate the hmac
+      digest = OpenSSL::Digest.new('sha256')
+      actual_hmac = OpenSSL::HMAC.digest(digest, key, ciphertext)
+      raise ArgumentError.new('HMAC mismatch') unless actual_hmac == hmac
+
+      # decrypt
+      cipher = OpenSSL::Cipher::Cipher.new(algorithm)
+      cipher.decrypt
+      cipher.iv = iv
+      cipher.key = OpenSSL::PKCS5.pbkdf2_hmac_sha1(key, salt, 2000, cipher.key_len)
+
+      plaintext = cipher.update(ciphertext)
+      plaintext << cipher.final
+      plaintext
+    end
+
+    private
+      def encode64(value)
+        Base64.encode64(value).gsub(/[\r\n]/, '')
+      end
+  end
 end
--- spec/conceal_spec.rb
+++ spec/conceal_spec.rb
@@ -1,7 +1,19 @@
 require 'spec_helper'
 
 describe Conceal do
+  let(:key) { SecureRandom.hex(128) }
+
   it 'has a version number' do
     expect(Conceal::VERSION).not_to be nil
+  end
+
+  it 'encrypt then decrypt returns the same original plaintext' do
+    expect(Conceal.decrypt(Conceal.encrypt('hello', key: key), key: key)).to eq('hello')
+  end
+
+  describe '#encrypt' do
+    it 'does not return the plaintext' do
+      expect(Conceal.encrypt('hello', key: key)).not_to eq('hello')
+    end
   end
 end
